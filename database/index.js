ffa7b2632a51489ffbb231794d64fbf4e24564e9
Joe Redmon <joesredmon@gmail.com>
2020-09-23 21:02:39 -0700

    added some sample data

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/rei_questions', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connect('mongodb://localhost/rei_questions', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const questionSchema = mongoose.Schema({
   question_id: Number,
@@ -13,7 +13,6 @@ const questionSchema = mongoose.Schema({
       id: Number,
       date: Number,
       body: String,
-      count: Number,
       user: String,
       helpful: {
         yes: Number,
@@ -25,4 +24,55 @@ const questionSchema = mongoose.Schema({
 
 const Question = mongoose.model('Question', questionSchema);
 
-module.exports = { Question };
+const sampleData = [
+        {
+    question_id: 058,
+    product_id: 017,
+    user: 'BobbyB',
+    date: "2019-01-31T10:01:00.000Z",
+    question_body: 'Does this product fit both men and women?',
+    answer: [
+        {
+        id: 023,
+        date: "2019-11-31T03:01:40.000Z",
+        body: 'Yes it does.',
+        user: 'REI Helper Sally',
+        helpful: {
+            yes: 0,
+            no: 1,
+        },
+        },
+    ],
+    }, 
+    {
+        question_id: 009,
+        product_id: 043,
+        user: 'Rachel098',
+        date: "2020-05-01T12:14:00.000Z",
+        question_body: 'How durable is this?',
+        answer: [
+            {
+            id: 023,
+            date: "2020-05-05T08:22:36.000Z",
+            body: 'It can stand up to all your favortie activities.',
+            user: 'REI Helper Jim',
+            helpful: {
+                yes: 2,
+                no: 0,
+            },
+            }, 
+            {
+                id: 079,
+                date: "2020-06-09T08:21:08.000Z",
+                body: 'It tore after one week.',
+                user: 'CaliBro43',
+                helpful: {
+                    yes: 1,
+                    no: 3,
+                },
+                },
+        ],
+        },
+]
+
+module.exports = { Question, db };
