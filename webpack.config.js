c3c29dc62cdd33d8bbef6321eb4d6047058a2134
Joe Redmon <joesredmon@gmail.com>
2020-09-25 19:49:39 -0700

    jest working, changed node from v8 to v10

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
-        test : /\.jsx?$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader : 'babel-loader',      
+        loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
-      }
-    ]
-  }
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
 };
