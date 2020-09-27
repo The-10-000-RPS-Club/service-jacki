56bbfc232e8838adf36def64e41a74fd384c9f59
Joe Redmon <joesredmon@gmail.com>
2020-09-27 11:15:55 -0700

    react skeleton

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,20 +4,32 @@ import ReactDOM from 'react-dom';
 
 import axios from 'axios';
 
-import Question from './components/Question.jsx';
-
 import QuestionList from './components/QuestionList.jsx';
 
 class App extends React.Component {
-constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+    };
+    this.getQuestions = this.getQuestions.bind(this);
+  }
+
+  componentDidMount() {
+    this.getQuestions();
+  }
+
+  getQuestions() {
+    axios.get('/api/products/questions')
+      .then((data) => this.setState({ questions: data }))
+      .catch((err) => console.log(err));
+}
 
   render() {
-    return(
-    <div><QuestionList /></div>
-    )
-   }
+    return (
+      <div><QuestionList /></div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
