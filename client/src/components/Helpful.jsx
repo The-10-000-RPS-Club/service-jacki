1d72aa8d5f30749bbde7ed165fe110216afbb968
Joe Redmon <joesredmon@gmail.com>
2020-09-30 10:42:36 -0700

    hooks working successfully, still need to fix patch method connection to front end

--- client/src/components/Helpful.jsx
+++ client/src/components/Helpful.jsx
@@ -1,26 +1,35 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect } from 'react';
 
 import styled from 'styled-components';
 
 const Helpful = ({ question, incrementHelpfulCount }) => {
-	const yes = question.answers[0].helpful.yes;
-	const no = question.answers[0].helpful.no;
-	const quesId = question.question_id;
-	const ansId = question.answers[0]._id;
+  const [quesId] = useState(question.question_id);
+  const [ansId] = useState(question.answers[0]._id);
+  const [yes, setYes] = useState(question.answers[0].helpful.yes);
+  const [no, setNo] = useState(question.answers[0].helpful.no);
 
-	return (
-		<p>Helpful? <HelpfulButton className='yes' onClick={() => incrementHelpfulCount(quesId, ansId, yes)}>yes: {yes}</HelpfulButton> <HelpfulButton className='no' onClick={() => incrementHelpfulCount(quesId, ansId, no)}>no: {no}</HelpfulButton> <HelpfulButton>Report as inappropriate</HelpfulButton> </p>
-	);
+  return (
+    <p>
+      Helpful? <HelpfulButton className='yes' onClick={() => {
+				incrementHelpfulCount(quesId, ansId, yes);
+				setYes(yes + 1);
+				}}>yes: {yes}</HelpfulButton> <HelpfulButton className='no' onClick={() => {
+					incrementHelpfulCount(quesId, ansId, no);
+					setNo(no + 1);
+					}}>no: {no}</HelpfulButton> <HelpfulButton>Report as inappropriate</HelpfulButton>
+    </p>
+  );
 };
 
 const HelpfulButton = styled.button`
-	background-color: white;
-	border: 1px solid grey;
-	border-radius: 2px;
-	&:hover {
+  background-color: white;
+  border: 1px solid grey;
+  border-radius: 2px;
+  &:hover {
     box-shadow: inset 0 0 3px #000000;
-	}
-	position: relative;
+  }
+  position: relative;
 `;
 
 export default Helpful;
