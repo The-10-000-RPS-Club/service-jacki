a50453dad7a1873bce024f37c17388cd07bb5000
Joe Redmon <joesredmon@gmail.com>
2020-10-02 13:39:56 -0700

    touch up helpful code

--- client/src/components/Helpful.jsx
+++ client/src/components/Helpful.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable no-underscore-dangle */
-import React, { useState, useEffect } from 'react';
-
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Helpful = ({ question, incrementHelpfulCount }) => {
@@ -11,25 +14,37 @@ const Helpful = ({ question, incrementHelpfulCount }) => {
 
   return (
     <p>
-      Helpful? <HelpfulButton className='yes' onClick={() => {
-				incrementHelpfulCount(quesId, ansId, 'yes');
-				setYes(yes + 1);
-				}}>Yes &#183; {yes}</HelpfulButton> <HelpfulButton className='no' onClick={() => {
-					incrementHelpfulCount(quesId, ansId, 'no');
-					setNo(no + 1);
-					}}>No &#183; {no}</HelpfulButton> <HelpfulButton>Report as inappropriate</HelpfulButton>
+      Helpful?
+      <HelpfulButton
+        className="yes"
+        onClick={() => {
+          incrementHelpfulCount(quesId, ansId, 'yes');
+          setYes(yes + 1);
+        }}>Yes &#183; {yes}
+      </HelpfulButton> <HelpfulButton
+        className="no"
+        onClick={() => {
+          incrementHelpfulCount(quesId, ansId, 'no');
+          setNo(no + 1);
+        }}>No &#183; {no}
+      </HelpfulButton> <HelpfulButton>Report as inappropriate</HelpfulButton>
     </p>
   );
 };
 
+Helpful.propTypes = {
+  question: PropTypes.shape().isRequired,
+  incrementHelpfulCount: PropTypes.func.isRequired,
+};
+
 const HelpfulButton = styled.button`
-  background-color: white;
-  border: 1px solid grey;
-	border-radius: 2px;
-	padding: 7px;
-  &:hover {
-		box-shadow: inset 0 0 3px #000000;
-		cursor: pointer;
+background-color: white;
+border: 1px solid grey;
+border-radius: 2px;
+padding: 7px;
+&:hover {
+box-shadow: inset 0 0 3px #000000;
+cursor: pointer;
   }
   position: relative;
 `;
