0c1c3fd8f277444b8c63bccc6d0021a7a40b6eb6
Joe Redmon <joesredmon@gmail.com>
2020-10-02 14:04:33 -0700

    touch up questionlist code

--- client/src/components/QuestionList.jsx
+++ client/src/components/QuestionList.jsx
@@ -1,15 +1,28 @@
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable import/extensions */
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import Question from './Question.jsx';
 
 const QuestionList = ({ questions, incrementHelpfulCount }) => (
   <div>
-	<Wrapper><p>1-{questions.length} of {questions.length} Questions</p></Wrapper>
-  {questions.map((question, i) => (
-    <Question question={question} key={i} incrementHelpfulCount={incrementHelpfulCount}/>
+    <Wrapper><p>1-{questions.length} of {questions.length} Questions</p></Wrapper>
+    {questions.map((question, i) => (
+      <Question
+        question={question}
+        key={i}
+        incrementHelpfulCount={incrementHelpfulCount} />
     ))}
   </div>
 );
+
+QuestionList.propTypes = {
+  questions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  incrementHelpfulCount: PropTypes.func.isRequired,
+};
 
 const Wrapper = styled.section`
 margin: auto;
