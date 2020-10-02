b46c6cf9e0cd2151c55e4cc3719d9fec9fddb935
Joe Redmon <joesredmon@gmail.com>
2020-10-02 13:28:06 -0700

    touch up code dropdown

--- client/src/components/DropDown.jsx
+++ client/src/components/DropDown.jsx
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from 'react';
-
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-import Helpful from './Helpful.jsx';
+function DropDown({ sort, setSort }) {
+  const [dropDownContent] = useState(['Newest questions', 'Newest answers', 'Most answered', 'Answers needed', 'Most helpful answers']);
 
-function DropDown({ questions, setQuestions, sort, setSort, }) {
-	const [dropDownContent] = useState(['Newest questions', 'Newest answers', 'Most answered', 'Answers needed', 'Most helpful answers']);
-	
+  return (
+    <div>
+      <NavbarDropdown>
+        <p>
+          Sort by: {sort} &#x25BE;
+        </p>
+        <NavbarDropdownContent>
+          <SingleDropdownOption onClick={() => { setSort('Newest questions'); }}><div>{dropDownContent[0]}</div></SingleDropdownOption>
+          <SingleDropdownOption onClick={() => { setSort('Newest answers'); }}><div>{dropDownContent[1]}</div></SingleDropdownOption>
+          <SingleDropdownOption onClick={() => { setSort('Most answered'); }}><div>{dropDownContent[2]}</div></SingleDropdownOption>
+          <SingleDropdownOption onClick={() => { setSort('Answers needed'); }}><div>{dropDownContent[3]}</div></SingleDropdownOption>
+          <SingleDropdownOption onClick={() => { setSort('Most helpful answers'); }}><div>{dropDownContent[4]}</div></SingleDropdownOption>
+        </NavbarDropdownContent>
+      </NavbarDropdown>
+    </div>
+  );
+}
 
-	return (
-		<div>
-		<NavbarDropdown>
-		<p>Sort by: {sort} &#x25BE;</p>
-		<NavbarDropdownContent>
-			<SingleDropdownOption onClick={() => {setSort('Newest questions')}}><div>{dropDownContent[0]}</div></SingleDropdownOption>
-				<SingleDropdownOption onClick={() => {setSort('Newest answers')}}><div>{dropDownContent[1]}</div></SingleDropdownOption>
-				<SingleDropdownOption onClick={() => {setSort('Most answered')}}><div>{dropDownContent[2]}</div></SingleDropdownOption>
-				<SingleDropdownOption onClick={() => {setSort('Answers needed')}}><div>{dropDownContent[3]}</div></SingleDropdownOption>
-				<SingleDropdownOption onClick={() => {setSort('Most helpful answers')}}><div>{dropDownContent[4]}</div></SingleDropdownOption>
-      </NavbarDropdownContent>
-	</NavbarDropdown>
-	</div>
-	);
+DropDown.propTypes = {
+  sort: PropTypes.string.isRequired,
+  setSort: PropTypes.func.isRequired,
 };
 
 const NavbarDropdownContent = styled.div`
@@ -44,7 +49,7 @@ padding-bottom: 5px;
 cursor: pointer;
 z-index: 9;
 &:hover ${NavbarDropdownContent} {
-	display: inline-block;
+display: inline-block;
 }
 font-family: "Roboto","Helvetica Neue","Helvetica","Arial",sans-serif;
 `;
@@ -52,10 +57,10 @@ font-family: "Roboto","Helvetica Neue","Helvetica","Arial",sans-serif;
 const SingleDropdownOption = styled.div`
 border: 10px solid white;
 &:hover {
-	background: #2B455C;;
-	color: white;
-	cursor: pointer;
-	border: 10px solid darkslategrey;
+background: #2B455C;;
+color: white;
+cursor: pointer;
+border: 10px solid darkslategrey;
 }
 `;
 
