dd30f20fc9c9e7189a4e2848eb5fe74985e60913
Joe Redmon <joesredmon@gmail.com>
2020-10-07 09:59:00 -0700

    minor changes

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://db:27017/rei_questions', { useFindAndModify: true, useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/rei_questions', { useFindAndModify: true, useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
