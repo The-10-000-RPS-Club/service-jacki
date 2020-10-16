db4c879086593045585fcb3a001dff20e64d6e06
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2020-10-15 22:41:11 -0700

    data generation script complete for small tests.

--- dataGenerator.js
+++ dataGenerator.js
@@ -1,27 +1,57 @@
 const fs = require('fs');
 const faker = require('faker');
+const { argv } = require('yargs');
+
+const lines = argv.lines || 1000;
 
 // create output files
 const fileProducts = fs.createWriteStream(__dirname + '/products_data.csv');
 const fileQuestions = fs.createWriteStream(__dirname + '/questions_data.csv');
 const fileAnswers = fs.createWriteStream(__dirname + '/answers_data.csv');
 
+const stream = fs.createWriteStream(fileProducts);
+
 // create a loop for number of questions to produce (multiple questions per product) Joe had 8 questions to 1 product ratio
 // 0-5 answers per question
+
+// for every product id, iterate over and count a random number of questions to leverage in question/product id reference.
+const getRandomId = () => {
+  for (let j = 1; j <= 100; j += 1) {
+    let numAnswersPerQ = Math.floor(Math.random() * 5) + 1;
+    for (let k = 0; k < numAnswersPerQ; k += 1) {
+      return j;
+    }
+  }
+}
+
 // create data to input into csv for questions
-const fakeQuestion = (count) => {
-  let question_id: count;
-  let created_at: faker.date.past(1);
-  let user: faker.internet.userName();
-  let question_body: faker.lorem.sentence();
-  let product_id:
-  //nested for loop (10M, then 0-5 questions assigned); export numbers for answers to reference during answer generation
-  //string.replace (reg X)
-  return `${question_id}, ${created_at}, ${user}, ${question_body}`;
+for (let i = 1; i <= 1000; i += 1) {
+  const fakeQuestion = (i, prod) => {
+    let question_id: i;
+    let created_at: faker.date.past(1);
+    let user: faker.internet.userName();
+    let question_body: faker.lorem.sentence();
+    let product_id: getRandomId();
+    //nested for loop (10M, then 0-5 questions assigned); export numbers for answers to reference during answer generation
+    //string.replace (reg X)
+    return `${question_id}, ${created_at}, ${user}, ${question_body}, ${product_id}`;
+  }
 }
 
 // create data to input into csv for answers
+for (let i = 1; i <= 5000; i += 1) {
+  const fakeAnswer = (i, question) => {
+    let answer_id: i;
+    let created_at: faker.date.past(1);
+    let user: faker.internet.userName();
+    let answer_body: faker.lorem.sentence();
+    let helpful_yes: Math.floor(Math.random() * 5);
+    let helpful_no: Math.floor(Math.random() * 5);
+    let question_id: getRandomId();
 
+    return `${answer_id}, ${created_at}, ${user}, ${answer_body}, ${helpful_yes}, ${helpful_no}, ${question_id} \n`;
+  }
+}
 
 
 // createwrite file stream for each csv (async/await)
@@ -32,7 +62,29 @@ for(let i=0; i<= 10; i++) {
 
 file.end();
 
-
+const writeOneMillionTimes = (writer, data, encoding, callback) => {
+  let i = lines;
+  function write() {
+    let ok = true;
+    do {
+      i--;
+      let product
+      if (i === 0) {
+        // Last time!
+        writer.write(data, encoding, callback);
+      } else {
+        // See if we should continue, or wait.
+        // Don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      // Had to stop early!
+      // Write some more once it drains.
+      writer.once('drain', write);
+    }
+  write();
+}
 
 
 
--- schema.sql
+++ schema.sql
@@ -1,23 +1,23 @@
 -- user joins for tables relationships
 
 CREATE TABLE products (
-  product_id INTEGER,
+  product_id INTEGER
 )
 
 CREATE TABLE questions (
   question_id INTEGER,
-  created_at TIME,
-  user VARCHAR,
-  question_body VARCHAR,
-  product_id INTEGER,
+  created_at TIMESTAMP,
+  user_name VARCHAR (50),
+  question_body VARCHAR (255),
+  product_id INTEGER
 )
 
 CREATE TABLE answers (
-  answer_id INTEGER PRIMARY KEY,
-  created_at TIME,
-  user VARCHAR,
-  answer_body VARCHAR,
+  answer_id INTEGER,
+  created_at TIMESTAMP,
+  user_name VARCHAR (50),
+  answer_body VARCHAR (255),
   helpful_yes INTEGER,
   helpful_no INTEGER,
-  question_id INTEGER,
+  question_id INTEGER
 )
